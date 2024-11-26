ba57d3b1262fa5fc7fe8a159c26fd9e5322772b6
benxta4424 <benxtaben@gmail.com>
2024-11-26 12:04:28 -0500

    creatng the posts model and tying it to the user

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  validates :user, presence: true
+  validates :title, presence: true, length: { maximum: 15 }
+  validates :body, presence: true, length: { minimum: 20 }
+end
